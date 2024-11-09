22378180b3ca5b208db3b0be6ad08563a041ea5b
AN.Mamun <96946518+anmamun0@users.noreply.github.com>
2024-11-09 12:14:03 +0600

    Delete transaction.py

--- transaction.py
+++
@@ -1,8 +0,0 @@
-class Transaction:
-    def __init__(self,action,amount,date,current_balance,after_balance) -> None:
-        self.action = action
-        self.amount = amount 
-        self.date = date 
-        self.current_balance = current_balance
-        self.after_balance = after_balance
-
